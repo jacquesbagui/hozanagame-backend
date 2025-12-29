23c2d603bfeef4f06008ecc9ba972d68707e01cf
Jean Jacques BAGUI <jacques.bagui@gmail.com>
2025-12-29 17:31:54 +0100

    update admin provider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -63,6 +63,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->authGuard('web')
+            ->authPasswordBroker('users');
     }
 }
